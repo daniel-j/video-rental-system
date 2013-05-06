0cbc68e45cee934fa58f00cce7244debc9b10525
Kevin Jouper <Kevin.Jouper@Gmail.com>
2013-05-06 12:36:35 +0200

    Created object of Handlers & Database

--- VideoRentalHandler/Controller/System.java
+++ VideoRentalHandler/Controller/System.java
@@ -1,13 +1,31 @@
+//Import all models
+import Model;
 
 public class System
 {
-
-	/**
-	 * @param args
-	 */
+	CustomerHandler CustomerH;
+	ItemHandler ItemH;
+	NewsletterHandler NewsletterH;
+	RentalHandler RentalH;
+	SearchHandler SearchH;
+	Database databse;
+	
+	//Construct
+	public System(){
+		//Init Handlers and Database
+		this.CustomerH 		= new CustomerHandler;
+		this.ItemH 			= new ItemHandler;
+		this.NewsletterH	= new NewsletterHandler;
+		this.RentalH 		= new RentalHandler;
+		this.SearchH 		= new SearchHandler;
+		this.databse 		= new Database;
+		//---------------------------------------
+	}
+	
+	}
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		
 
 	}
 
