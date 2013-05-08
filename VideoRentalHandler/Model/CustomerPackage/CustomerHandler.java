0a4439565ae86acc365262fa779ca2ff333a3515
Henrik Nordin <henknordin@gmail.com>
2013-05-08 13:14:38 +0200

    Updated CustomerHandler.java

--- VideoRentalHandler/Model/CustomerPackage/CustomerHandler.java
+++ VideoRentalHandler/Model/CustomerPackage/CustomerHandler.java
@@ -1,14 +1,73 @@
 //In this package
 package Model.CustomerPackage;
 
+import java.util.Vector;
+
+import Model.ItemPackage.Item;
+
 //Import what this class use
 
-public class CustomerHandler {
+public class CustomerHandler
+{	
+	private Vector<Customer> customers;
 	
 	//Constructor
-	public CustomerHandler(){
+	public CustomerHandler()
+	{
+		this.customers = new Vector<Customer>();
+	}
+	
+	public boolean addCustomer(String name, String ssn, String address, String preference)
+	{
+		this.customers.add(new Customer(name, ssn, address, preference));
 		
+		return true;
+	}
+	
+	public boolean editCustomer(int id, String name, String ssn, Vector<Item> rentedItems, String address, String preference)
+	{
+		this.customers.elementAt(id).changeInfo(name, ssn, rentedItems, address, preference);
 		
+		return true;
+	}
+	
+	public boolean removeCustomer(int id)
+	{
+		this.customers.remove(id);
+		
+		return true;
+	}
+	
+	public Customer getCustomer(int id)
+	{
+		return this.customers.elementAt(id);
+	}
+	
+	public boolean setItemToCustomer(int id, Item item)
+	{
+		this.customers.elementAt(id).addItem(item);
+		
+		return true;
+	}
+	
+	public Vector<String> getCustomersWithPreferences(String preference)
+	{
+		Vector<String> addressList = new Vector<String>();
+		
+		for(Customer temp : this.customers)
+		{
+			if(temp.getPreference().equals(preference))
+			{
+				addressList.add(temp.getAddress());
+			}
+		}
+		
+		return addressList;
+	}
+	
+	public Vector<Customer> getListOfCustomers()
+	{
+		return this.customers;
 	}
 	
 }
